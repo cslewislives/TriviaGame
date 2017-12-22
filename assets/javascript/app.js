2a3acb6e7a6e5446c201f3887ed8f19811e88cff
cslewislives <cslewislives@gmail.com>
2017-12-22 03:15:13 -0500

    finished pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+//player presses start and the first question appears
+//there is a certain amount of time per question
+//player has multiple choices to choose from
+//when player chooses an option player is shown correct answer and if they got it right
+//with no input the app goes to the next question
+//clock is reset
+//if player does not choose an answer in the time given player is shown correct answer
+//once all questions are finished player is shown amount correct/incorrect/unanswered
+//timer is stopped
+//player can click reset button
+//button restarts game but does not reload the page.
+
+
+
+
+})
